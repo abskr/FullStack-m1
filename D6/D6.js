c22afa0535895bda94cdf5b4f2434084e958f802
ABskr <bskr26@gmx.de>
2021-02-09 09:02:13 +0100

    D6.js created

---
+++ D6/D6.js
@@ -0,0 +1,11 @@
+function changeHeadingsText(){
+    let heading = document.querySelector("h1")
+    //console.log(heading)
+    heading.innerText = "It's actually amazon.de"
+}
+
+function changeBodysBackgoundColor(){
+    let background = document.querySelector("body")
+    background.style.backgroundColor = 'green'
+}
+
